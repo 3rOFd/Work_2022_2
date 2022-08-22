a28f576e32c892a7ea07c1b636cb917d08d53691
3rOFd <mihinm08@mail.ru>
2022-08-22 17:43:24 +1000

    Исправил код

--- Lecture_2_1/main.cpp
+++ Lecture_2_1/main.cpp
@@ -9,12 +9,8 @@ int main()
     //For ease of reading, I will write out the divisible and divide separately
     printf("Enter the argument x: ");
     scanf("%lf",&x);
-    if (x<=1) printf("Error");
-    else
-    {
-        y1=2*cos(x-M_PI/6)+sqrt(2);
-        y2=1/(2*log(x))+sin(pow(2,x))*sin(pow(2,x));
-        Y=y1/y2*exp(3*x);
-        printf("Answer: %lf\n",Y);
-    }
+    y1=2*cos(x-M_PI/6)+sqrt(2);
+    y2=1/(2*log(x))+sin(pow(2,x))*sin(pow(2,x));
+    Y=y1/y2*exp(3*x);
+    (x<=1)? printf("Error\n"): printf("Answer: %lf\n",Y);
 }
