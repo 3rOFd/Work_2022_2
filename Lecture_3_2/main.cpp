2c90afa26bf58dd5c04dbaf7bdb8ace675e3e95c
3rOFd <mihinm08@mail.ru>
2022-08-28 22:55:52 +0300

    Написал код ко 2 заданию 3ей лекции (присутствует небольшое сомнение о правильной работе программы)

---
+++ Lecture_3_2/main.cpp
@@ -0,0 +1,29 @@
+/*
+    Task: Given a natural number N. Write a program to determine the number of digits in this number.
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Please enter x, then y and z: ");
+    int x,y,z;
+    scanf ("%d %d %d",&x, &y, &z);
+    if(x<=0 || y<=0 || z<=0) printf("Error");
+    else
+    {
+        int n = x>y && x>z? x:y>z? y:z;
+        /*
+            Often the smallest common divisor is found in the initial stages of number decomposition,
+            the larger the numbers become, the less likely to meet the smallest common divisor,
+            if it is found, the counter will still go to the largest number, which eliminates the loss
+
+        */
+        int k=0;
+        for(int i=1; i<n; ++i)
+        {
+            if(x%i==0 && y%i==0 && z%i==0) k=i;
+        }
+        printf("Answer: %d\n", k);
+    }
+}
