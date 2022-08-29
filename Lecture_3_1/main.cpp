ca00f1c2a9f8dc87f07f7521d7ed63005a44c301
3rOFd <mihinm08@mail.ru>
2022-08-29 18:25:12 +0300

    Написал код к 1 заднию 3ей лекции

--- Lecture_3_1/main.cpp
+++ Lecture_3_1/main.cpp
@@ -6,24 +6,18 @@
 
 int main()
 {
-    printf("Please enter x, then y and z: ");
-    int x,y,z;
-    scanf ("%d %d %d",&x, &y, &z);
-    if(x<=0 || y<=0 || z<=0) printf("Error");
+    printf("Please enter N: ");
+    int N;
+    scanf ("%d", &N);
+    if(N <= 0) printf("Error");
     else
     {
-        int n = x>y && x>z? x:y>z? y:z;
-        /*
-            Often the smallest common divisor is found in the initial stages of number decomposition,
-            the larger the numbers become, the less likely to meet the smallest common divisor,
-            if it is found, the counter will still go to the largest number, which eliminates the loss
-
-        */
-        int k=0;
-        for(int i=1; i<n; ++i)
+        int amount = 0;
+        while(N > 0)
         {
-            if(x%i==0 && y%i==0 && z%i==0) k=i;
+            ++amount;
+            N /= 10;
         }
-        printf("Answer: %d\n", k);
+        printf("Answer: %d\n", amount);
     }
 }
