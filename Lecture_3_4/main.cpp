dc8d3fc65a494d1307ff4048978fb9daa1f21d76
3rOFd <mihinm08@mail.ru>
2022-08-29 18:37:40 +0300

    Написал код к 4 заданию 3ей лекции

--- Lecture_3_4/main.cpp
+++ Lecture_3_4/main.cpp
@@ -10,9 +10,9 @@ int main()
     printf("Enter the number N: ");
     int n, answer = 0;
     scanf("%d", &n);
-    for(int i=0; n>0; ++i)
+    for(int i = 0; n > 0; ++i)
     {
-        answer += (n%2)*pow(10,i);
+        answer += (n % 2) * pow(10, i);
         n /= 2;
     }
     printf("Answer: %d\n", answer);
