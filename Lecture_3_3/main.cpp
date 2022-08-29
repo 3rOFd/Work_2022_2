1f554e18944c438abc8cc2ec3ee12a7a085d90bb
3rOFd <mihinm08@mail.ru>
2022-08-29 18:35:54 +0300

    Написал код к 3 заданию 3ей лекции

--- Lecture_3_3/main.cpp
+++ Lecture_3_3/main.cpp
@@ -8,25 +8,25 @@
 int main()
 {
     printf("Enter the number N: ");
-    int N,i = 0;
-    scanf("%d",&N);
-    int P = N,k = 0;
-    for(; P>0; ++i)
+    int N, i = 0;
+    scanf("%d", &N);
+    int P = N, k = 0;
+    for(; P > 0; ++i)
     {
-        P/=10;
-        if(P%10 == 0) ++k;
+        P /= 10;
+        if(P % 10 == 0) ++k;
     }
-    int x = N%10;
-    int y = N/(pow(10,i-1));
-    int midle = (N-y*pow(10,i-1))/10;
-    if(k>1)
+    int x = N % 10;
+    int y = N / (pow(10, i - 1));
+    int midle = (N - y * pow(10, i - 1)) / 10;
+    if(k > 1)
     {
-        printf("Answer: %d",x);
-        for(; k>1; k--) printf("0");
-        printf("%d\n",y);
+        printf("Answer: %d", x);
+        for(; k > 1; k--) printf("0");
+        printf("%d\n", y);
     }
-    else if(i == 2) printf("Answer: %d%d\n",x,y);
-    else if(i == 1) printf("Answer: %d",i);
-    else printf("Answer: %d%d%d\n",x,midle,y);
+    else if(i == 2) printf("Answer: %d%d\n", x, y);
+    else if(i == 1) printf("Answer: %d", i);
+    else printf("Answer: %d%d%d\n", x, midle, y);
 
 }
