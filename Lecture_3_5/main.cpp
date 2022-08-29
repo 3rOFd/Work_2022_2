f3b15719ef0ac45412bd0dc6dc2c44be70e2204d
3rOFd <mihinm08@mail.ru>
2022-08-29 18:04:36 +0300

    Написал код к 5 заданию 3ей лекции

--- Lecture_3_5/main.cpp
+++ Lecture_3_5/main.cpp
@@ -1,5 +1,5 @@
 /*
-    Task: the sum of the row with an accuracy of 10^-5, where 0.05<x<1
+    Task: the sum of the row with an accuracy of 10^-5, where 0.05 < x < 1
 */
 
 #include<math.h>
@@ -8,40 +8,40 @@
 
 int main()
 {
-    printf("Enter the x in the range 0.05<x<1: ");
+    printf("Enter the x in the range 0.05 < x < 1: ");
     double x, eps = 1e-5;
     double result = 0;
-    double amount = 0;
+    double amount = 1;
     scanf("%lf", &x);
 
     if(x <= 0.05 || x >= 1) printf("Error");
     else
     {
         int n = 1;
+        double first_value_even = 1;
         double last_value_even = 1;
-        double last = 1;
-        double second_last_even = 1;
-        double l_last = 1;
+        double first_last_odd = 1;
+        double last_value_odd = 1;
         while(true)
         {
             if(n % 2 == 0)
             {
-                auto divided = ((2 * n - 1) * last_value_even * pow(x, 2 * n + 1));
-                auto divider = double(((2 * n) * last * (2 * n + 1)));
+                auto divided = ((2 * n - 1) * first_value_even * pow(x, 2 * n + 1));
+                auto divider = double(((2 * n) * last_value_even * (2 * n + 1)));
                 result = divided / divider;
-                last_value_even = 2 * n - 1;
-                last = 2 * n;
+                first_value_even = 2 * n - 1;
+                last_value_even = 2 * n;
                 if(result < eps) break;
                 amount += result;
                 ++n;
             }
             else
             {
-                auto divided = ((2 * n - 1) * second_last_even * pow(x, 2 * n + 1));
-                auto divider = double(((2 * n) * l_last * (2 * n + 1)));
+                auto divided = ((2 * n - 1) * first_last_odd * pow(x, 2 * n + 1));
+                auto divider = double(((2 * n) * last_value_odd * (2 * n + 1)));
                 result = divided / divider;
-                second_last_even = 2 * n - 1;
-                l_last = 2 * n;
+                first_last_odd = 2 * n - 1;
+                last_value_odd = 2 * n;
                 if(result < eps) break;
                 amount += result;
                 ++n;
