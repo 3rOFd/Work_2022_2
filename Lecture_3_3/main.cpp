5fa1930592b5db3ebbeb5c56cf2d0b7dbc7406ad
3rOFd <mihinm08@mail.ru>
2022-08-30 14:50:21 +0300

    Исправил код

--- Lecture_3_3/main.cpp
+++ Lecture_3_3/main.cpp
@@ -11,10 +11,12 @@ int main()
     int N, i = 0;
     scanf("%d", &N);
     int P = N, k = 0;
-    for(; P > 0; ++i)
+    while(P > 0)
     {
         P /= 10;
         if(P % 10 == 0) ++k;
+        ++i;
+
     }
     int x = N % 10;
     int y = N / (pow(10, i - 1));
@@ -22,11 +24,30 @@ int main()
     if(k > 1)
     {
         printf("Answer: %d", x);
-        for(; k > 1; k--) printf("0");
+        while(k > 1)
+        {
+            printf("0");
+            --k;
+
+        }
         printf("%d\n", y);
+
     }
-    else if(i == 2) printf("Answer: %d%d\n", x, y);
-    else if(i == 1) printf("Answer: %d", i);
-    else printf("Answer: %d%d%d\n", x, midle, y);
+    else if(i == 2)
+    {
+        printf("Answer: %d%d\n", x, y);
+
+    }
+    else if(i == 1)
+    {
+        printf("Answer: %d", i);
+
+    }
+    else
+    {
+        printf("Answer: %d%d%d\n", x, midle, y);
+
+    }
+    return 0;
 
 }
