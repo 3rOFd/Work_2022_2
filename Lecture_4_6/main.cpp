0ec0e80b421299c01d51131ee1d6fced9f40889c
3rOFd <mihinm08@mail.ru>
2022-09-08 08:29:54 +0300

    Не работает программа для некоторых случаев

---
+++ Lecture_4_6/main.cpp
@@ -0,0 +1,31 @@
+/*
+    A character string is given. Determine if it is a valid parentheses expression.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    constexpr int s = 40;
+    char M[s];
+    gets(M);
+    int n = 0;
+    int i = 0;
+    while(M[i] != 0)
+    {
+        if(M[i] == '(' || ')')
+        {
+            ++n;
+        }
+        ++i;
+    }
+    if(n % 2 == 0 )
+    {
+        printf("correct parentheses");
+    }
+    else
+    {
+        printf("incorrect brackets");
+    }
+    return 0;
+}
