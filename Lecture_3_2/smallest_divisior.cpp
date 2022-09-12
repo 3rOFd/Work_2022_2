47e8b28b9cd8d83a265d9b164e52a2f5243227f0
3rOFd <mihinm08@mail.ru>
2022-09-12 10:19:16 +0300

    а

---
+++ Lecture_3_2/smallest_divisior.cpp
@@ -0,0 +1,23 @@
+#include "smallest_divisior.h"
+#include<stdio.h>
+void smallest_divisior(int x, int y, int z)
+{
+    if(x <= 0 || y <= 0 || z <= 0) printf("Error");
+    else
+    {
+        int n = x > y && x > z ? x : y > z ? y : z;
+        /*
+            Often the smallest common divisor is found in the initial stages of number decomposition,
+            the larger the numbers become, the less likely to meet the smallest common divisor,
+            if it is found, the counter will still go to the largest number, which eliminates the loss
+
+        */
+        int k = 0;
+        for(int i = 1; i < n; ++i)
+        {
+            if(x % i == 0 && y % i == 0 && z % i == 0) k = i;
+        }
+        printf("Answer: %d\n", k);
+    }
+
+}
---
+++ Lecture_3_2/smallest_divisior.h
@@ -0,0 +1,4 @@
+#ifndef SMALLEST_DIVISIOR_H
+#define SMALLEST_DIVISIOR_H
+void smallest_divisior(int x, int y, int z);
+#endif // SMALLEST_DIVISIOR_H
