f17e8d67cf02162d70be826202c205d90652f9e6
3rOFd <mihinm08@mail.ru>
2022-09-12 18:14:26 +0300

    Написал код к 4 заданию 3ей лекции

--- Lecture_3_4/main.cpp
+++ Lecture_3_4/main.cpp
@@ -2,21 +2,17 @@
     Task: Translate a number from decimal to binary.
 */
 
+#include"binary.h"
 #include<math.h>
 #include<stdio.h>
 
 int main()
 {
     printf("Enter the number N: ");
-    int n, answer = 0;
+    int n;
+    int answer = 0;
     scanf("%d", &n);
     int i = 0;
-    while(n > 0)
-    {        
-        answer += (n % 2) * pow(10, i);
-        n >>= 1;
-        ++i;
-
-    }
-    printf("Answer: %d\n", answer);
+    to_binary(n, answer, i);
+    return 0;
 }
