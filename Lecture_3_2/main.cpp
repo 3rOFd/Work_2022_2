75079345c4363b1e234c443e6101480b0888b1a1
3rOFd <mihinm08@mail.ru>
2022-09-12 10:18:02 +0300

    Написал код ко 2 заданию 3ей лекции

--- Lecture_3_2/main.cpp
+++ Lecture_3_2/main.cpp
@@ -3,6 +3,7 @@
     (1 will be considered the smallest common divisor only if the given numbers have no other common divisors).
 */
 
+#include"smallest_divisior.h"
 #include<stdio.h>
 
 int main()
@@ -10,21 +11,6 @@ int main()
     printf("Please enter x, then y and z: ");
     int x, y, z;
     scanf ("%d %d %d", &x, &y, &z);
-    if(x <= 0 || y <= 0 || z <= 0) printf("Error");
-    else
-    {
-        int n = x > y && x > z ? x : y > z ? y : z;
-        /*
-            Often the smallest common divisor is found in the initial stages of number decomposition,
-            the larger the numbers become, the less likely to meet the smallest common divisor,
-            if it is found, the counter will still go to the largest number, which eliminates the loss
-
-        */
-        int k = 0;
-        for(int i = 1; i < n; ++i)
-        {
-            if(x % i == 0 && y % i == 0 && z % i == 0) k = i;
-        }
-        printf("Answer: %d\n", k);
-    }
+    smallest_divisior(x, y, z);
+    return 0;
 }
